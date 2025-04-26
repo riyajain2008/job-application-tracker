079ac7945f9836826fefc925a38b02f86196d0c6
Riya Jain <72738274+riyajain2008@users.noreply.github.com>
2025-04-25 21:59:57 -0400

    Delete CodingJobs directory

--- CodingJobs/apps/job/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Job, Application
-
-admin.site.register(Job)
-admin.site.register(Application)
--- CodingJobs/apps/userprofile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Userprofile
-
-admin.site.register(Userprofile)
